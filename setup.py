86fd01f0a1ea7494891801f67f637105300d4c87
patlewig <patlewig@hotmail.com>
2023-09-16 19:22:31 -0400

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Manuscript summarising the categorisation efforts for PFAS.',
+    author='patlewig',
+    license='MIT',
+)
